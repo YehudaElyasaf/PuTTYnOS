0eb9a715931e60c5aaf017bda953e0b9fc14e836
g33king <turhuhxckuo@gmail.com>
2022-11-09 21:51:03 +0200

    Rewrite GDT

--- boot/gdt.asm
+++ boot/gdt.asm
@@ -1,58 +1,33 @@
-gdt_start: ; don't remove the labels, they're needed to compute sizes and jumps
-    ; the GDT starts with a null 8-byte
-    dd 0x0 ; 4 byte
-    dd 0x0 ; 4 byte
+gdt_start:
+    dq 0 ; start with 8 null bytes
 
-; GDT for code segment. base = 0x00000000, length = 0xfffff
-; for flags, refer to os-dev.pdf document, page 36
-gdt_code: 
-    dw 0xffff    ; segment length, bits 0-15
-    dw 0x0       ; segment base, bits 0-15
-    db 0x0       ; segment base, bits 16-23
-    db 10011010b ; flags (8 bits)
-    db 11001111b ; flags (4 bits) + segment length, bits 16-19
-    db 0x0       ; segment base, bits 24-31
-
-; GDT for data segment. base and length identical to code segment
-; some flags changed, again, refer to os-dev.pdf
-gdt_data:
-    dw 0xffff
-    dw 0x0
-    db 0x0
-    db 10010010b
+gdt_code_segment:
+    dw 0xffff    ; limit = all 4 GB
+    dw 0         ; part of base adress
+    db 0         ; part of base adress
+    ; access byte. Accessed, ReadableWriteable, Dont conform lower ring access,
+    ; Code segment, not system segment, User ring (2 bits), Present
+    db 01011111b
+    ; flags and limit (4 bits): Not long(not 64bit), Reserved, Count limit in kibibytes (go up to 4 MiB), 32bit segment
     db 11001111b
-    db 0x0
+    db 0         ; continue of base address
+
+gdt_data_segment:
+    dw 0xffff    ; limit = all 4 GB
+    dw 0         ; part of base adress
+    db 0         ; part of base adress
+    ; access byte. Accessed, ReadableWriteable, Count from start up,
+    ; Data segment, not system segment, User ring (2 bits), Present
+    db 01001111b
+    ; flags and limit (4 bits): Not long(not 64bit), Reserved, Count limit in kibibytes (go up to 4 MiB), 32bit segment
+    db 11001111b
+    db 0         ; continue of base address
 
 gdt_end:
 
-; GDT descriptor
 gdt_descriptor:
-    dw gdt_end - gdt_start - 1 ; size (16 bit), always one less of its true size
-    dd gdt_start ; address (32 bit)
+    dw gdt_end - gdt_start - 1 ; length of gdt table (minus 1 because starting at 0)
+    dd gdt_start               ; start of gdt table
 
-; define some constants for later use
-CODE_SEG equ gdt_code - gdt_start
-DATA_SEG equ gdt_data - gdt_start
-
-[bits 16]
-switch_to_pm:
-    cli ; 1. disable interrupts
-    lgdt [gdt_descriptor] ; 2. load the GDT descriptor
-    mov eax, cr0
-    or eax, 0x1 ; 3. set 32-bit mode bit in cr0
-    mov cr0, eax
-    jmp CODE_SEG:init_pm ; 4. far jump by using a different segment
-
-[bits 32]
-init_pm: ; we are now using 32-bit instructions
-    mov ax, DATA_SEG ; 5. update the segment registers
-    mov ds, ax
-    mov ss, ax
-    mov es, ax
-    mov fs, ax
-    mov gs, ax
-
-    mov ebp, 0x90000 ; 6. update the stack right at the top of the free space
-    mov esp, ebp
-
-    call BEGIN_PM ; 7. Call a well-known label with useful code
+CODE_SEGMENT equ gdt_code_segment - gdt_start
+DATA_SEGMENT equ gdt_data_segment - gdt_start
