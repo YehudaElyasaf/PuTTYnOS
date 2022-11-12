5d3762fa96d1540a050bc66abe518420eb9e00df
Yehuda Elyasaf <hvusvvnkl12345@gmail.com>
2022-11-13 01:12:02 +0200

    wrote initIdtEntry()

--- kernel/idt.c
+++ kernel/idt.c
@@ -1,5 +1,7 @@
 #include "idt.h"
 
+#define KERNEL_CODE_SEGMENT     0x8
+
 IDTEntry idt[NUM_OF_IDT_ENTRIES];
 IDTR idtr = {sizeof(idt) - 1, idt};
 
@@ -9,4 +11,13 @@ void initIdt(){
         : //no output
         : "r" (&idtr)
     );
+}
+
+void initIdtEntry(uint8_t entryNum, uint32_t isrAdress, uint8_t flags){
+    IDTEntry* entry = &idt[entryNum];
+    entry->offsetLow = (uint16_t)isrAdress;             //casting uses 16 LSBs
+    entry->offsetHigh = (uint16_t)(isrAdress >> 16);    //>> 16 = casting uses 16 MSB
+    entry->reserved = 0x0;
+    entry->segmentSelector = KERNEL_CODE_SEGMENT;
+    entry->flags = flags;
 }
--- kernel/idt.h
+++ kernel/idt.h
@@ -2,21 +2,27 @@
 
 #include <stdint.h>
 
-#define NUM_OF_IDT_ENTRIES 256
+#define NUM_OF_IDT_ENTRIES      256
+
+/*
+    FLAGS (MSB to LSB):
+        present                 1 bit   always 1
+        DPL (privilege level)   2 bits  
+        zero bit                1 bit   always zero (beimash'lahem lama tsarich et ze?)
+        gate type               4 bits  0101 - task gate, 1110 - 32bit interrupt gate, 1111 - 32bit trap gate
+*/
+#define IDT_FLAGS_TASK_GATE             0b10000101
+#define IDT_FLAGS_INTERRUPT_GATE_RING0  0b10001110
+#define IDT_FLAGS_TRAP_GATE_RING0       0b10001111
+#define IDT_FLAGS_INTERRUPT_GATE_RING3  0b11101110
+#define IDT_FLAGS_TRAP_GATE_RING3       0b11101111
 
 typedef struct
 {
     uint16_t offsetLow; //bits 0-15
     uint16_t segmentSelector;
     uint8_t reserved; //always 0x0
-    /*
-    FLAGS (MSB to LSB):
-        present                 1 bit   always 1
-        DPL (privilege level)   2 bits
-        zero bit                1 bit   always zero (beimash'lahem lama tsarich et ze?)
-        gate type               4 bits  details above
-    */
-    uint8_t flags;
+    uint8_t flags; //detailed structure above
     uint16_t offsetHigh; //bits 16-31
 } __attribute__((packed)) IDTEntry;
 
@@ -28,11 +34,4 @@ typedef struct
 
 
 void initIdt();
-
-/*
-0x5 - task gate
-0x6 - 16bit interrupt gate
-0x7 - 16bit trap gate
-0xe - 32bit interrupt gate
-0xf - 32bit trap gate
-*/
+void initIdtEntry(uint8_t entryNum, uint32_t isrAdress, uint8_t flags);
