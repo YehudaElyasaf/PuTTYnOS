1a074be878a6132644b7bfc1316cb2c34b98ed5d
Yehuda Elyasaf <hvusvvnkl12345@gmail.com>
2022-11-29 23:54:10 +0200

    created cpu folder

---
+++ kernel/cpu/asm.h
@@ -0,0 +1,8 @@
+#pragma once
+
+//Set interrup flag
+void sti();
+//Clear interrup flag
+void cli();
+//Do nothing until next interrupt
+void hlt();
---
+++ kernel/cpu/isr.c
@@ -0,0 +1,8 @@
+#include "isr.h"
+#include "asm.h"
+#include "../io/print.h"
+
+
+void isrHandler(registers_t r){
+    kcprint("An isr was called!\n", RED, getDefaultBackgroundColor());
+}
