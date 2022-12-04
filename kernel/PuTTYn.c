ca78cc00ee7d68209c6ba3b4661297d498a4f6aa
Yehuda Elyasaf <hvusvvnkl12345@gmail.com>
2022-12-04 03:09:07 +0200

    consflict

--- kernel/PuTTYn.c
+++ kernel/PuTTYn.c
@@ -45,10 +45,6 @@ void initialize(){
 void main(){
     initialize();
    
-<<<<<<< HEAD
-=======
-    kprint("\nRunning shell!\n");
->>>>>>> 973261848ec06f88d22d5806950093c01ca80d77
     shellMain();
 }
 #endif
@@ -59,12 +55,6 @@ void main(){
     initialize();
 
     kprinti(syscall(SYSCALL_PRINT, 1, 2, 3, 4));
-<<<<<<< HEAD
-
-=======
-    
-    kprint("\nRunning shell!\n");
->>>>>>> 973261848ec06f88d22d5806950093c01ca80d77
     shellMain();
 }
 #endif
--- user/shell.c
+++ user/shell.c
@@ -3,10 +3,6 @@
 #include "../kernel/io/screen.h"
 
 void shellMain(){
-<<<<<<< HEAD
-    kprint("\nRunning shell!\n");
-=======
->>>>>>> 973261848ec06f88d22d5806950093c01ca80d77
     clearScreen();
     printPuTTYnOS(0);
 }
