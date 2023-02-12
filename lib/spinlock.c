30f55c15b664b95055ff47768d944b0304900683
g33king <turhuhxckuo@gmail.com>
2023-02-12 18:11:38 +0200

    debug spinlock

--- lib/spinlock.c
+++ lib/spinlock.c
@@ -3,20 +3,13 @@
 void spin_lock(Spinlock *lock) {
     unsigned char prev;
     asm volatile(
-        "1: xorl $0x1, %[prev]\n\t"
+        "1: xorb $0x1, %[prev]\n\t"
         "lock cmpxchgb %[prev], %[lock]\n\t"
         "jne 1b\n\t"
-        : [prev] "=q" (prev), [lock] "+m" (lock->lock)
-        :
-        : "memory"
+        : [prev] "=q" (prev), [lock] "+m" (lock->lock) :: "memory"
     );
 }
 
 void spin_unlock(Spinlock *lock) {
-    asm volatile(
-        "movl $0, %[lock]\n\t"
-        : [lock] "+m" (lock->lock)
-        :
-        : "memory"
-    );
+    asm volatile("movl $0, %[lock]\n\t" : [lock] "+m" (lock->lock) :: "memory");
 }
