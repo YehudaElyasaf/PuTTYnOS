5a30928e79a3dbb0b2c7f1c4a181c8caf8c705c0
Yehuda Elyasaf <hvusvvnkl12345@gmail.com>
2023-03-18 20:22:06 +0200

    pci readwrite

--- kernel/network/nic/rtl8139.c
+++ kernel/network/nic/rtl8139.c
@@ -50,9 +50,10 @@ bool initRTL8139(NetwotkAdapter* nic){
         kprint("\tFound device: RTL8139\n");
     }
 
-    uint32_t pciCommand = PCI_Read(pciAddr+0x4);
-    pciCommand |= 1 << 2; 
-    PCI_Write(pciAddr, pciCommand+0x4);
+    uint32_t pciCommand = PCI_Read(pciAddr + 0x4);
+    pciCommand |= (1 << 2);
+    PCI_Write(pciAddr + 0x4, pciCommand);
+    printf("PCICMD: %x\n", pciCommand);
 
     rx_buffer = buffer;
     tx_buffer = buffer + RX_BUFFER_LEN;
